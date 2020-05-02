cc5a05403b6c61ee0a25bba205918ac819bc0e57
haoyun <haoyun.tw@gmail.com>
2020-05-03 01:12:07 +0800

    Initial

---
+++ app/src/main/java/tw/haoyun/logback_android_file_appender/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.haoyun.logback_android_file_appender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
