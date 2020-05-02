fb6c7c0ed77d99d2df85a7766d3f0162e534375a
haoyun <haoyun.tw@gmail.com>
2020-05-03 01:46:14 +0800

    Add logback.xml: log in SD card

--- app/src/main/java/tw/haoyun/logback_android_file_appender/MainActivity.java
+++ app/src/main/java/tw/haoyun/logback_android_file_appender/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class MainActivity extends AppCompatActivity {
+    private final static Logger LOG = LoggerFactory.getLogger(MainActivity.class);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LOG.error("Error Hello World");
+        LOG.warn("Warn Hello World");
+        LOG.debug("Debug Hello World");
+        LOG.info("Info Hello World");
+        LOG.trace("Trace Hello World");
     }
 }
